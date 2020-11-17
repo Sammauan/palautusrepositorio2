4a708bc5f5e3e7c5373e44c97d8169e5b3631b99
Sammauan <samki1000@hotmail.com>
2020-11-17 16:23:48 +0200

    frontti Herokuun

---
+++ build/precache-manifest.e861ff05765be59c79e2b5c6dd813dd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe69abde41cf38f04a17a8a496d61376",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63b991065a1cb423ef01",
+    "url": "/static/css/main.76a79b54.chunk.css"
+  },
+  {
+    "revision": "8f4f45adc222c096b17f",
+    "url": "/static/js/2.a03bcb81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a03bcb81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b991065a1cb423ef01",
+    "url": "/static/js/main.af3caee4.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
